182059f1057c4a518616d34701814e4a361d5814
Florian <flo@mzechmeister.de>
2021-12-16 19:55:57 +0100

    Support multiple stacks and removing them first

---
+++ bin/deploy.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+source $(dirname "$0")/utils.sh
+
+if [ $1 ]; then
+  _get_env
+
+  for var in "$@"; do
+    case $var in
+      "nextcloud"|"nc")
+        container=$(docker ps -qf "name=nextcloud_app")
+
+        if [ $container ]; then
+          _input "nextcloud is already running. Remove stack first? (y/n) "
+          read -r answer
+          if [ "$answer" = "y" ]; then
+            docker stack remove nextcloud
+
+            # wait for deletion
+            network=$(docker network ls -qf "name=nextcloud")
+            echo "waiting for networks to be removed"
+            while [ "$network" ]; do
+              network=$(docker network ls -qf "name=nextcloud")
+            done
+          fi
+        fi
+
+        env ENV=$env HOST=$host docker stack deploy -c nextcloud.yml nextcloud --with-registry-auth
+        ;;
+      "edge")
+        env ENV=$env docker stack deploy -c edge.yml edge
+        ;;
+      "keycloak"|"kc")
+        env ENV=$env HOST=$host docker stack deploy -c keycloak.yml keycloak --with-registry-auth
+        ;;
+      *)
+        _input "choose service from: nextcloud, edge, keycloak ..."
+        ;;
+    esac
+  done
+fi
