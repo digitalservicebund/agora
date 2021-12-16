044499650122b140d3a9197c63158aca4d6302b8
Florian <flo@mzechmeister.de>
2021-12-16 18:29:05 +0100

    Save local env settings

--- bin/utils.sh
+++ bin/utils.sh
@@ -7,9 +7,20 @@ _input() {
 }
 
 _get_env() {
-  _input "What's the environment? (test, staging, prod): "
-  # check if shit already exists
-  read -r env 
-  _input "What's your host's domain? (including subdomain): "
-  read -r host
+  CONF_FILE=bin/local.env
+
+  if [ -f "$CONF_FILE" ]; then
+    source $CONF_FILE
+  else
+    _input "What's the environment? (test, staging, prod): "
+    read -r env 
+    _input "What's your host's domain? (including subdomain): "
+    read -r host
+
+    cat > $CONF_FILE << EOF
+# local settings
+env=$env
+host=$host
+EOF
+  fi
 }
