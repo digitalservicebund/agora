7e14d18d2aef5e32c24f36a413bbbabc672b52d1
Florian <flo@mzechmeister.de>
2022-01-05 18:23:04 +0100

    Create backup folder if necessary

--- bin/upgrade_forum.sh
+++ bin/upgrade_forum.sh
@@ -6,4 +6,7 @@
 docker exec -it $(docker ps -f name=forum -q) discourse backup
 
 # backup discourse's backup files 
-docker cp $(docker ps -f name=forum -q):/var/www/discourse/public/backups/default/ ../agora_backups/discourse
+# check if folder exists first
+BACKUP_DIR=../agora_backups/discourse
+[ ! -d "$BACKUP_DIR" ] && echo "backup folder does not exist, creating one..." && mkdir $BACKUP_DIR
+docker cp $(docker ps -f name=forum -q):/var/www/discourse/public/backups/default/ $BACKUP_DIR
